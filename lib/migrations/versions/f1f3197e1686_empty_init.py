a130b1f6f0e232d0e5328927e296d09ffe06cb3d
Isaac Song <isaacsongs10@gmail.com>
2023-11-12 17:44:36 -0800

    Migrations and alembic complete

---
+++ lib/migrations/versions/f1f3197e1686_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f1f3197e1686
+Revises: 
+Create Date: 2023-11-12 17:38:48.355975
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f1f3197e1686'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
